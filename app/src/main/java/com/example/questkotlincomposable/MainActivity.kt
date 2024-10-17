6f412ffefc9de07814b11637dd2a33bb69e734f9
emzhran <zachran750@gmail.com>
2024-10-17 16:10:12 +0700

    Penambahan code untuk menampilkan gambar (logo umy) yang telah disimpan di folder drawable

--- app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
+++ app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -45,6 +48,7 @@ fun BasicKotlin(modifier: Modifier = Modifier) {
             style = TextStyle(fontSize = 50.sp)
         )
         Text(text = "Ini adalah halaman login")
+        Image(painter = painterResource(id = R.drawable.umy), contentDescription = "")
     }
 }
 
