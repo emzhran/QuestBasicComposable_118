280da5c7459b40bda4811da4313e387948e0bb83
emzhran <zachran750@gmail.com>
2024-10-17 16:14:50 +0700

    Menambahkan jarak dan juga mengatur size untuk gambar (logo umy)

--- app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
+++ app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +19,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questkotlincomposable.ui.theme.QuestKotlinComposableTheme
 
@@ -48,7 +50,7 @@ fun BasicKotlin(modifier: Modifier = Modifier) {
             style = TextStyle(fontSize = 50.sp)
         )
         Text(text = "Ini adalah halaman login")
-        Image(painter = painterResource(id = R.drawable.umy), contentDescription = "")
+        Image(painter = painterResource(id = R.drawable.umy), contentDescription = "", Modifier.padding(top = 40.dp).size(200.dp))
     }
 }
 
