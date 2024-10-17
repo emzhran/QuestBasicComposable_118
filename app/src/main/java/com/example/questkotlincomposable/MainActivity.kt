bc48e14f472b53b50e2085569ea921a7ae199db8
emzhran <zachran750@gmail.com>
2024-10-17 15:51:24 +0700

    Menambahkan fungsi composable BasicKotlin yang menampilkan 2 teks yaitu UMYEAH dan juga Hallow

--- app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
+++ app/src/main/java/com/example/questkotlincomposable/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questkotlincomposable.ui.theme.QuestKotlinComposableTheme
@@ -31,11 +34,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicKotlin(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(text = "UMYEAH")
+        Text(text = "Hallow")
+    }
 }
 
 @Preview(showBackground = true)
